4476e534f24079c381668490781184258a0e8395
Enstrue <Ens.true@gmail.com>
2024-03-10 21:07:59 +0300

    second step project

--- gendiff.js
+++ gendiff.js
@@ -6,5 +6,8 @@ const program = new Command();
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .option('-v, --version', 'output the version number');
+  .option('-v, --version', 'output the version number')
+  .option('-f, --format [type]', 'output format')
+  .argument('<filepath1>', 'path for first file')
+  .argument('<filepath2>', 'path for second file');
 program.parse();
