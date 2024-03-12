3497b3afcea17a35794af33b7f0b153b46bd7866
Enstrue <Ens.true@gmail.com>
2024-03-12 15:55:25 +0300

    modify 3rd step

--- src/gendiff.js
+++ src/gendiff.js
@@ -1,4 +1,7 @@
 import { Command } from 'commander';
+import fs from 'fs';
+import path from 'path';
+import parseFile from './parse';
 
 const program = new Command();
 
@@ -8,8 +11,28 @@ program
   .option('-v, --version', 'output the version number')
   .option('-f, --format [type]', 'output format')
   .argument('<filePath1>', 'path for first file')
-  .argument('<filePath2>', 'path for second file');
-program.parse(process.argv);
+  .argument('<filePath2>', 'path for second file')
+  .action((filePath1, filePath2) => {
+    // получаю пути к фаилам
+    const absolutePath1 = path.resolve(filePath1);
+    const absolutePath2 = path.resolve(filePath2);
 
-const [filePath1, filePath2] = program.args;
-export { filePath1, filePath2 };
+    // читаю фаилы для дальнейшего парсинга
+    const dataFromFirstFile = fs.readFileSync(absolutePath1, 'utf-8');
+    const dataFromSecondFile = fs.readFileSync(absolutePath2, 'utf-8');
+
+    // определю формат
+    const firstFileExtention = path.extname(absolutePath1);
+    const secondFileExtention = path.extname(absolutePath2);
+
+    // получаем распарсенные данные
+    const { parsedFirstFile, parsedSecondFile } = parseFile(
+      dataFromFirstFile,
+      dataFromSecondFile,
+      firstFileExtention,
+      secondFileExtention,
+    );
+    // логика отвечающая за сравнение файлов
+  });
+
+program.parse(process.argv);
--- src/parse.js
+++ src/parse.js
@@ -1,16 +1,19 @@
-import fs from 'fs';
-import path from 'path';
-import { filePath1, filePath2 } from '../gendiff.js';
+const parseFile = (
+  dataFromFirstFile,
+  dataFromSecondFile,
+  firstFileExtention,
+  secondFileExtention,
+) => {
+  let parsedFirstFile;
+  let parsedSecondFile;
 
-const parseFile = () => {
-  const absolutePath1 = path.resolve(filePath1);
-  const absolutePath2 = path.resolve(filePath2);
+  if (firstFileExtention === '.json') {
+    parsedFirstFile = JSON.parse(dataFromFirstFile);
+  }
 
-  const dataFromFirstFile = fs.readFileSync(absolutePath1, 'utf-8');
-  const dataFromSecondFile = fs.readFileSync(absolutePath2, 'utf-8');
-
-  const parsedFirstFile = JSON.parse(dataFromFirstFile);
-  const parsedSecondFile = JSON.parse(dataFromSecondFile);
+  if (secondFileExtention === '.json') {
+    parsedSecondFile = JSON.parse(dataFromSecondFile);
+  }
 
   return { parsedFirstFile, parsedSecondFile };
 };
